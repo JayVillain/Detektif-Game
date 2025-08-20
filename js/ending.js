e9235499b4c7324d9dc791dfce2b4c21832559d1
JayVillain <saipi3825@gmail.com>
2025-08-20 16:28:42 +0000

    Aug 20, 2025, 11:28 PM

---
+++ js/ending.js
@@ -0,0 +1,18 @@
+const ending = {
+  trueEnding: {
+    speaker: "Narator",
+    text: "Mesin waktu menyala. Zain menoleh terakhir kali: 'Terima kasih... meski jalan ini pahit, akhirnya aku bisa pulang. Pulang pada Gracia.'",
+    after: `
+      <div style="text-align:center; margin-top:20px;">
+        <img src="assets/img/gracia.png" style="max-height:300px;"><br>
+        <p><em>
+        "Cinta sejati bukan tentang waktu yang mudah,<br>
+        melainkan keberanian melawan waktu itu sendiri.<br>
+        Zain rela menantang masa depan demi kembali ke masa lalu.<br>
+        Dan di sana, dalam pelukan Gracia... ia akhirnya pulang."
+        </em></p>
+        <h2>The End</h2>
+      </div>
+    `
+  }
+};
